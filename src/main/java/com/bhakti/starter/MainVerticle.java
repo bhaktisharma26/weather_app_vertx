dbe7262fd2e9ad05606eb56e3be5853af14db379
Bhakti <sharmabhakti2611@gmail.com>
2024-08-26 13:52:14 +0530

    first commit1

---
+++ src/main/java/com/bhakti/starter/MainVerticle.java
@@ -0,0 +1,34 @@
+package com.bhakti.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.JWTAuthHandler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.http.HttpClient;
+import org.slf4j.Logger;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!");
+    }).listen(8888).onComplete(http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
